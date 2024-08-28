fdff3a871925fb3ab10856f4ab2f5e388c4a42de
Yaswanth Myneni <79623546+yaswanthmyneni@users.noreply.github.com>
2024-08-28 17:52:52 +0530

    Modified navigation section

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -4,10 +4,27 @@ import "./navigation.css";
 
 const Navigation = () => {
   return (
-    <nav className='nav'>
+    <nav className="nav">
       <Link to="/">
-        <h1>Ghoomanewala</h1>
+        <h1 className="color_skyblue">Ghoomanewala</h1>
       </Link>
+      <div className="nav_items">
+        <p>
+          <Link className="color_skyblue" to="/about-us">
+            About Us
+          </Link>
+        </p>
+        <p>
+          <Link className="color_skyblue" to="/services">
+            Services
+          </Link>
+        </p>
+        <p>
+          <Link className="color_skyblue" to="/contact-us">
+            Contact Us
+          </Link>
+        </p>
+      </div>
     </nav>
   );
 };
